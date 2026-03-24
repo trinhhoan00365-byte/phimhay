ec20e53296401673738c20782891c87a169303f1
trinhhoan00365-byte <trinhhoan00365@gmail.com>
2026-03-24 16:40:32 +0700

    Update watch.js

--- js/watch.js
+++ js/watch.js
@@ -1,5 +1,5 @@
 const AFF_LINK = "https://broadlyjukeboxunrevised.com/2058173";
-const AFF_ENABLED = false; 
+const AFF_ENABLED = true; 
 const WORKER_URL = "https://go.avboy.top";
 
 let id = null;
